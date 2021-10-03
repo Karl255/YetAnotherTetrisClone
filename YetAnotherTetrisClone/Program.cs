ab492ddbd099805f655ed03d893eebdf677355ba
Karlo <49595438+Karl255@users.noreply.github.com>
2021-10-03 14:35:39 +0200

    Initial commit

---
+++ YetAnotherTetrisClone/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NBodySim
+{
+	public static class Program
+	{
+		[STAThread]
+		static void Main()
+		{
+			using var game = new YetAnotherTetrisCloneGame();
+			game.Run();
+		}
+	}
+}
---
+++ YetAnotherTetrisClone/YetAnotherTetrisCloneGame.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace NBodySim
+{
+	public class YetAnotherTetrisCloneGame : Game
+	{
+		private GraphicsDeviceManager Graphics;
+		private SpriteBatch SpriteBatch;
+		private Vector2 ScreenSize = new(0, 0);
+
+		private SpriteFont GameFont;
+		private MouseState PreviousMouseState;
+		private KeyboardState PreviousKeyboardState;
+
+		public YetAnotherTetrisCloneGame()
+		{
+			Graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+			IsMouseVisible = true;
+		}
+
+		protected override void Initialize()
+		{
+			ScreenSize.X = Graphics.PreferredBackBufferWidth = 100;
+			ScreenSize.Y = Graphics.PreferredBackBufferHeight = 300;
+			Graphics.ApplyChanges();
+
+			base.Initialize();
+		}
+
+		protected override void LoadContent()
+		{
+			SpriteBatch = new SpriteBatch(GraphicsDevice);
+			GameFont = Content.Load<SpriteFont>("gameFont");
+		}
+
+		protected override void Update(GameTime gameTime)
+		{
+			var mouseState = Mouse.GetState();
+			var keyboardState = Keyboard.GetState();
+
+			PreviousMouseState = mouseState;
+			PreviousKeyboardState = keyboardState;
+
+			base.Update(gameTime);
+		}
+
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.Black);
+
+			SpriteBatch.Begin();
+			SpriteBatch.End();
+
+			base.Draw(gameTime);
+		}
+	}
+}
